fbfbe2c8c8f685885d5a5df40a5cc4f54b440abd
AccountHyzo <AccountHyzo@hotmail.com>
2020-10-02 19:30:11 +0200

    Added all items to the distribution file Added  icons for all the magazines and cleaned up some of the existing icons.

--- media/lua/client/Ammocraft_Recipe.lua
+++ media/lua/client/Ammocraft_Recipe.lua
@@ -2,18 +2,19 @@ function recipe_Dismantle_Ammo(items, result, player)
 local inv = player:getInventory();
 for i=0, items:size()-1 do
 		local item = items:get(i)
-		inv:AddItem("Ammocraft." .. item:getType() .. "_casing_noprimer");
-		inv:AddItem("Ammocraft." .. item:getType() .. "_tip");
-		if item:getType() == "Bullets9mm" or item:getType() == "Bullets38" then
+		local ammo =  item:getType()
+		inv:AddItem("Ammocraft." .. ammo .. "_casing");
+		inv:AddItem("Ammocraft." .. ammo .. "_tip");
+		if item:getType() == "Bullets9mm" or ammo == "Bullets38" then
 		inv:AddItem("Ammocraft.SP_Primers");
 		end
-		if item:getType() == "Bullets44" or item:getType() == "Bullets45" then
+		if item:getType() == "Bullets44" or ammo == "Bullets45" then
 		inv:AddItem("Ammocraft.LP_Primers");
 		end
-		if item:getType() == "223Bullets" or item:getType() == "308Bullets" or item:getType() == "556Bullets" then
+		if ammo == "223Bullets" or ammo == "308Bullets" or ammo == "556Bullets" then
 		inv:AddItem("Ammocraft.R_Primers");
 		end
-		if item:getType() == "ShotgunShells" then
+		if ammo == "ShotgunShells" then
 		inv:AddItem("Ammocraft.SG_Primers");
 		end
 	end
@@ -27,11 +28,42 @@ for i=0, items:size()-1 do
 	end
 end
 
+function recipe_Remove_Primer(items, result, player)
+local inv = player:getInventory();
+for i=0, items:size()-1 do
+		local item = items:get(i)
+		inv:AddItem("Ammocraft." .. item:getType() .. "_spent_noprimer");
+	end
+end
+
 function recipe_Assemble_Casing(items, result, player)
 local inv = player:getInventory();
 for i=0, items:size()-1 do
 		local item = items:get(i)
-		local firstitem = items:get(1)
-		inv:AddItem("Ammocraft." .. item:getType() .. "_noprimer");
+		local ammo = item:getType()
+		if ammo == "Ammocraft.Bullets9mm_casing_spent_noprimer" then
+		inv:AddItem("Ammocraft.Bullets9mm_casing");
+		end
+		if ammo == "Ammocraft.Bullets38_casing_spent_noprimer" then
+		inv:AddItem("Ammocraft.Bullets38_casing");
+		end
+		if ammo == "Ammocraft.Bullets44_casing_spent_noprimer" then
+		inv:AddItem("Ammocraft.Bullets44_casing");
+		end
+		if ammo == "Ammocraft.Bullets45_casing_spent_noprimer" then
+		inv:AddItem("Ammocraft.Bullets45_casing");
+		end
+		if ammo == "Ammocraft.223Bullets_casing_spent_noprimer" then
+		inv:AddItem("Ammocraft.223Bullets_casing");
+		end
+		if ammo == "Ammocraft.308Bullets_casing_spent_noprimer" then
+		inv:AddItem("Ammocraft.308Bullets_casing");
+		end
+		if ammo == "Ammocraft.556Bullets_casing_spent_noprimer" then
+		inv:AddItem("Ammocraft.556Bullets_casing");
+		end
+		if ammo == "Ammocraft.ShotgunShells_casing_spent_noprimer" then
+		inv:AddItem("Ammocraft.ShotgunShells_casing");
+		end
 	end
 end
--- media/lua/shared/Reloading/Ammocraft_Reloading.lua
+++ media/lua/shared/Reloading/Ammocraft_Reloading.lua
@@ -1,80 +1,22 @@
 require "Reloading/ISReloadableWeapon"
 require "Reloading/ISReloadableMagazine"
 
+ISSemiAutoWeapon =  ISReloadableWeapon:derive("ISSemiAutoWeapon");
+
 --************************************************************************--
 --** 							Firing
 --************************************************************************--
 
 function ISSemiAutoWeapon:fireShot(weapon, difficulty)
-	print("Pow!")
+	print("ISSemiAutoWeapon:fireShot")
 	self.roundChambered = 0;
 	if self.currentCapacity > 0 then
 		self.currentCapacity = self.currentCapacity - 1;
 		self.roundChambered = 1;
 	end
-	if char:getCurrentSquare() then
-		local round = InventoryItemFactory.CreateItem('Ammocraft.' .. self.ammoType .. '_casing')
-		if round then
-			char:getCurrentSquare():AddWorldInventoryItem(round, 0, 0, 0)
-			ISInventoryPage.dirtyUI()
-		end
-	end
 	self:syncReloadableToItem(weapon)
-end
-
-function ISShotgunWeapon:fireShot(weapon, difficulty)
-	print("Pow!")
-	self.roundChambered = 0;
-	self.emptyShellChambered = 1;
-	self:syncReloadableToItem(weapon);
-end
-
-function ISReloadableWeapon:fireShot()
-	-- do nothing
-	print("ISReloadableWeapon:fireShot")
 end
 
 --************************************************************************--
 --** 							Racking
 --************************************************************************--
-
-function ISShotgunWeapon:rackingPerform(char, square, weapon)
-	print("Rack em")
-	if self.roundChambered == 1 and char:getCurrentSquare() then
-		local round = InventoryItemFactory.CreateItem(self.moduleName .. '.' .. self.ammoType)
-		if round then
-			char:getCurrentSquare():AddWorldInventoryItem(round, 0, 0, 0)
-			ISInventoryPage.dirtyUI()
-		end
-	elseif self.emptyShellChambered == 1 and char:getCurrentSquare() then
-		local round = InventoryItemFactory.CreateItem('Ammocraft.' .. self.ammoType .. '_casing')
-		if round then
-			char:getCurrentSquare():AddWorldInventoryItem(round, 0, 0, 0)
-			ISInventoryPage.dirtyUI()
-		end
-	end
-    self.roundChambered = 0;
-	self.emptyShellChambered = 0
-    if(self.currentCapacity > 0) then
-        self.currentCapacity = self.currentCapacity - 1;
-        self.roundChambered = 1;
-	end
-	self:syncReloadableToItem(weapon);
-end
-
-function ISSemiAutoWeapon:rackingPerform(char, square, weapon)
-	if self.roundChambered == 1 and char:getCurrentSquare() then
-		local clipData = ReloadUtil:getClipData(self.ammoType)
-		local round = InventoryItemFactory.CreateItem(clipData.moduleName .. '.' .. clipData.ammoType)
-		if round then
-			char:getCurrentSquare():AddWorldInventoryItem(round, 0, 0, 0)
-			ISInventoryPage.dirtyUI()
-		end
-	end
-    self.roundChambered = 0;
-    if(self.currentCapacity > 0) then
-        self.currentCapacity = self.currentCapacity - 1;
-        self.roundChambered = 1;
-	end
-	self:syncReloadableToItem(weapon);
-end
