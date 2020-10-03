90d7e50758b18e57b18380f35cda969c374a03ee
AccountHyzo <AccountHyzo@hotmail.com>
2020-10-03 18:37:36 +0200

    Added spent cases on weapon fire

---
+++ media/lua/shared/Reloading/ammocraft_reloading.lua
@@ -0,0 +1,19 @@
+require "ISBaseObject"
+require "Reloading/ISReloadUtil"
+require "Reloading/ISRackAction"
+require "Reloading/ISReloadAction"
+
+local function spawnCasing(wielder, weapon)
+
+local player = wielder;
+
+local weapon = weapon;
+local gun = weapon:getType();
+local gunAmmo, replaced = string.gsub(weapon:getAmmoType(), "Base.", "")
+print(gunAmmo)
+	if weapon:getSubCategory() == "Firearm" and weapon:isRoundChambered() == true and weapon:isJammed() == false then
+			player:getCurrentSquare():AddWorldInventoryItem("Ammocraft." .. gunAmmo .. "_casing_spent", 0.0, 0.0, 0.0);
+	end
+end
+
+Events.OnWeaponSwing.Add(spawnCasing);
