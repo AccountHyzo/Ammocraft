3eec2c6121e01c719bc01db52e76fb511b0ae1bf
AccountHyzo <AccountHyzo@hotmail.com>
2020-10-03 19:41:08 +0200

    Hotfix! Lua Error on attack

--- media/lua/shared/Reloading/ammocraft_reloading.lua
+++ media/lua/shared/Reloading/ammocraft_reloading.lua
@@ -8,11 +8,12 @@ local function spawnCasing(wielder, weapon)
 	local player = wielder;
 	local weapon = weapon;
 	local gun = weapon:getType();
-	local gunAmmo, replaced = string.gsub(weapon:getAmmoType(), "Base.", "")
 
 	if weapon:getSubCategory() == "Firearm" and weapon:isRoundChambered() == true and weapon:isJammed() == false then
+		local gunAmmo, replaced = string.gsub(weapon:getAmmoType(), "Base.", "")
 		player:getCurrentSquare():AddWorldInventoryItem("Ammocraft." .. gunAmmo .. "_casing_spent", 0.0, 0.0, 0.0);
 	elseif gun == "Revolver_Short"  or gun == "Revolver" or gun == "Revolver_Long" then
+		local gunAmmo, replaced = string.gsub(weapon:getAmmoType(), "Base.", "")
 		if weapon:getSubCategory() == "Firearm" and weapon:getCurrentAmmoCount() > 0 and weapon:isJammed() == false then
 			player:getCurrentSquare():AddWorldInventoryItem("Ammocraft." .. gunAmmo .. "_casing_spent", 0.0, 0.0, 0.0);
 		end
