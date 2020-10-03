f7f10998c0a299a73ec0e8c4759a8a929aa8163c
AccountHyzo <AccountHyzo@hotmail.com>
2020-10-03 19:24:47 +0200

    HOTFIX: Casings spawned even though no round in chamber of non-revolver guns. FIXED

--- media/lua/shared/Reloading/ammocraft_reloading.lua
+++ media/lua/shared/Reloading/ammocraft_reloading.lua
@@ -5,15 +5,17 @@ require "Reloading/ISReloadAction"
 
 local function spawnCasing(wielder, weapon)
 
-local player = wielder;
+	local player = wielder;
+	local weapon = weapon;
+	local gun = weapon:getType();
+	local gunAmmo, replaced = string.gsub(weapon:getAmmoType(), "Base.", "")
 
-local weapon = weapon;
-local gun = weapon:getType();
-local gunAmmo, replaced = string.gsub(weapon:getAmmoType(), "Base.", "")
 	if weapon:getSubCategory() == "Firearm" and weapon:isRoundChambered() == true and weapon:isJammed() == false then
+		player:getCurrentSquare():AddWorldInventoryItem("Ammocraft." .. gunAmmo .. "_casing_spent", 0.0, 0.0, 0.0);
+	elseif gun == "Revolver_Short"  or gun == "Revolver" or gun == "Revolver_Long" then
+		if weapon:getSubCategory() == "Firearm" and weapon:getCurrentAmmoCount() > 0 and weapon:isJammed() == false then
 			player:getCurrentSquare():AddWorldInventoryItem("Ammocraft." .. gunAmmo .. "_casing_spent", 0.0, 0.0, 0.0);
-	elseif weapon:getSubCategory() == "Firearm" and weapon:getCurrentAmmoCount() > 0 and weapon:isJammed() == false then
-			player:getCurrentSquare():AddWorldInventoryItem("Ammocraft." .. gunAmmo .. "_casing_spent", 0.0, 0.0, 0.0);
+		end
 	end
 end
 
