59299b4c16c800e3a50311d81c5ded77225fbd4c
AccountHyzo <AccountHyzo@hotmail.com>
2020-10-04 05:48:47 +0200

    New Casing icons Bugfix - Double barreled not spawning casings

--- media/lua/shared/Reloading/ammocraft_reloading.lua
+++ media/lua/shared/Reloading/ammocraft_reloading.lua
@@ -12,7 +12,7 @@ local function spawnCasing(wielder, weapon)
 	if weapon:getSubCategory() == "Firearm" and weapon:isRoundChambered() == true and weapon:isJammed() == false then
 		local gunAmmo, replaced = string.gsub(weapon:getAmmoType(), "Base.", "")
 		player:getCurrentSquare():AddWorldInventoryItem("Ammocraft." .. gunAmmo .. "_casing_spent", 0.0, 0.0, 0.0);
-	elseif gun == "Revolver_Short"  or gun == "Revolver" or gun == "Revolver_Long" then
+	elseif gun == "Revolver_Short"  or gun == "Revolver" or gun == "Revolver_Long" or gun == "DoubleBarrelShotgun" then
 		local gunAmmo, replaced = string.gsub(weapon:getAmmoType(), "Base.", "")
 		if weapon:getSubCategory() == "Firearm" and weapon:getCurrentAmmoCount() > 0 and weapon:isJammed() == false then
 			player:getCurrentSquare():AddWorldInventoryItem("Ammocraft." .. gunAmmo .. "_casing_spent", 0.0, 0.0, 0.0);
